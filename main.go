e1f1255186eb46609593dd9d242ffca9fb141fc1
Akmaljon <ajalilov@transparent.com>
2021-11-10 12:11:26 +0500

    -

--- main.go
+++ main.go
@@ -2,24 +2,39 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"google.golang.org/grpc"
 	"log"
+	"time"
 	"velox/gRPC/recommendation"
 )
 
 func main() {
-	channel, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
+	ticker := time.NewTicker(time.Second * 1)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+
+	go func() {
+		sec := 0
+		for _ = range ticker.C {
+			sec++
+			fmt.Println("Sec: ", sec)
+
+		}
+	}()
+	channel, err := grpc.DialContext(ctx, "localhost:50055", grpc.WithInsecure())
 	if err != nil {
-		log.Fatalln( err)
+		log.Fatalln(err)
 	}
 	client := recommendation.NewRecommendationsClient(channel)
-	res, err := client.Recommend(context.Background(), &recommendation.RecommendationRequest{
+	res, err := client.Recommend(ctx, &recommendation.RecommendationRequest{
 		UserId:     1,
 		Category:   recommendation.BookCategory_SCIENCE_FICTION,
 		MaxResults: 3,
 	})
+	ticker.Stop()
+	cancel()
 	if err != nil {
-		log.Fatalln( err)
+		log.Fatalln(err)
 	}
 	log.Printf("outliers at: %v", res.Recommendations)
 }
--- python-server/main.py
+++ python-server/main.py
@@ -9,16 +9,16 @@ import recommendations_pb2_grpc as recommendations_pb2_grpc
 
 class Recommendation(recommendations_pb2_grpc.RecommendationsServicer):
     def Recommend(self, request, context):
-        return recommendations_pb2.RecommendationResponse(recommendations=[
-            recommendations_pb2.BookRecommendation(id=1, title="The Maltese Falcon"),
-            recommendations_pb2.BookRecommendation(id=2, title="Murder on the Orient Express"),
-            recommendations_pb2.BookRecommendation(id=3, title="The Hound of the Baskervilles"),
-        ])
+        recommendations = []
+        for i in range(0, 100, 1):
+            recommendations.append(recommendations_pb2.BookRecommendation(id=1, title="The Maltese Falcon"))
+        return recommendations_pb2.RecommendationResponse(recommendations=recommendations)
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     recommendations_pb2_grpc.add_RecommendationsServicer_to_server(Recommendation(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('[::]:50055')
     server.start()
     server.wait_for_termination()
 
